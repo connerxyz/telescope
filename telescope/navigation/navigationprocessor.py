c7c0920d6d06aad5326febe9fdcb5bede5e778b6
Conner Cowling <conner.cowling@thomsonreuters.com>
2019-05-10 16:08:10 -0500

    added back to top argument to navigation processor.

--- telescope/navigation/navigationprocessor.py
+++ telescope/navigation/navigationprocessor.py
@@ -10,19 +10,21 @@ log = logging.getLogger()
 
 
 class NavigationProcessor(ProcessorInterface):
-
     NAVTAG = "<telescope-nav></telescope-nav>"
+    NAVNAME="nav"
+    TOPLINK = '<a href="#{}">*back to top*</a>\n'.format(NAVNAME)
 
     def __init__(self, name="navigation"):
         super().__init__(name)
 
-    def transform(self, notebook: Notebook, depth=3, *args, **kwargs) -> Notebook:
+    def transform(self, notebook: Notebook, depth=3, back_to_top=True,
+                  *args, **kwargs) -> Notebook:
         """
         Generate navigation section for given notebook.
         """
         navsection = [
-            self.NAVTAG, # Allows for detecting preexisting nav section
-            '<a name="top">*Back to top*</a>\n',
+            self.NAVTAG,  # Affords detecting preexisting nav section
+            '<a name="{}"></a>\n'.format(self.NAVNAME),  # Affords linking to nav section
             '## Navigation\n',
             '\n'
         ]
@@ -40,6 +42,8 @@ class NavigationProcessor(ProcessorInterface):
                             fragid = notebooks.fragmentid(line)
                             # Map heading into navsection
                             navsection.append(navlink(hlevel, line, fragid))
+                            # Add link to nav section based on flag
+                            if back_to_top: scp.insert(i, self.TOPLINK)
                             # Insert anchor just before current line in source copy
                             scp.insert(i, anchorlink(fragid))
                 # Overwrite the cell's source with the transformed source
